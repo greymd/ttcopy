0d4f29d9574ca0ae41fcccfe845df4580b96e37b
mecab <mecab@misosi.ru>
2017-01-16 18:30:17 +0900

    Let it paste the last paste when failed to get the link

--- wpb.sh
+++ wpb.sh
@@ -4,12 +4,20 @@
 # ほげほげ
 
 MYSTR="myclip"
+LASTPASTE_PATH="${TMPDIR}/lastPaste"
 wpbcopy () {
     local TRANS_URL=$(curl -so- --upload-file <(cat) https://transfer.sh/$MYSTR);
     curl -s -X POST "https://cl1p.net/$MYSTR" --data "content=$TRANS_URL" > /dev/null
 }
 
 wpbpaste () {
-    local TRANS_URL=$(curl -s "https://cl1p.net/$MYSTR" | xmllint --html --xpath '/html/body/div/div/textarea/text()' -)
-    curl -so- "$TRANS_URL"
+    local TRANS_URL=$(curl -s "https://cl1p.net/$MYSTR" | xmllint --html --xpath '/html/body/div/div/textarea/text()' -) 2> /dev/null || ""
+    if [ "$TRANS_URL" = "" ]; then
+        [ -f $LASTPASTE_PATH ] || return 1
+        echo "(Pasting the last paste)" >&2
+        cat "$LASTPASTE_PATH"
+        return 0
+    fi
+    curl -so- "$TRANS_URL" | tee "$LASTPASTE_PATH"
 }
+
